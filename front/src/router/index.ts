018d73f48cde35ae3a10bee83d7c925e82802202
Randriantahina <shanjeev24.apollon@gmail.com>
2026-05-20 13:01:32 +0300

    Add routing and views for login, registration, and dashboard

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -2,7 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue'),
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue'),
+    },
+  ],
 })
 
 export default router
---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <div classname="p-4 min-h-screen flex flex-col items-center justify-center">
+    <h1>Login</h1>
+    <form>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
